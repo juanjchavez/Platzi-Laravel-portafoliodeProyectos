a49a129ac700f24117ad1a114b83539a834c1a24
Juan Chavez <juan.chavezn1990@gmail.com>
2021-03-10 22:20:40 -0600

    Creacion de componente livewire para index de cursos

--- resources/views/layouts/web.blade.php
+++ resources/views/layouts/web.blade.php
@@ -7,10 +7,26 @@
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
     </head>
     <body>
+        <header class="shadow-lg">
+            <div class="bg-blue-900 py-1"></div>
+            <nav class="bg-blue-800 py-2">
+                <a href="{{ route('home') }}">
+                    <img src=" {{ asset('images/isotipo.png') }}" class="h-8 mx-auto">
+                </a>
+            </nav>
+        </header>
         <main class="py-10">
             <div class="container mx-auto px-4">
                 @yield('content')
             </div>
         </main>
+        <footer class="py-4 text-center">
+            @auth
+                <a href="{{url('dashboard')}}" class="text-sm text-gray-700 underline">Dashboard</a>
+            @else
+                <a href="{{url('login')}}" class="text-sm text-gray-700 underline">login</a>
+                <a href="{{url('register')}}" class="text-sm text-gray-700 underline">register</a>
+            @endauth
+        </footer>
     </body>
 </html>
